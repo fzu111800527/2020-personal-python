dd40a747cd1b68ad2a8b96483c97486290b2df09
fzu111800527 <55120996+fzu111800527@users.noreply.github.com>
2020-09-16 20:53:13 +0800

    Update GHAnalysis.py

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -5,17 +5,18 @@ import os
 
 
 def readjson(addr):
-    filelist = os.listdir(addr)#读取文件列表
-    f2=open('data.json','w',encoding= 'utf-8')
-    for file in filelist:#读json文件
+    filelist = os.listdir(addr)  # 读取文件列表
+    f2 = open('data.json', 'w', encoding='utf-8')
+    for file in filelist:  # 读json文件
         if file[-5:] == '.json':
             pathname = addr + '\\' + file
-            with open(pathname,encoding='utf-8') as f:
+            with open(pathname, encoding='utf-8') as f:
                 for line in f:
-                    f2.write(line)#写入data.json
+                    f2.write(line)  # 写入data.json
     return
 
-def calculate_result(data,username,reponame,eventname):
+
+def calculate_result(data, username, reponame, eventname):
     count = 0
     for line in data:
         if not len(username) == 0:
@@ -32,13 +33,13 @@ def main():
     username = ''
     reponame = ''
     eventname = ''
-    options, args = getopt.getopt(sys.argv[1:], 'i:u:r:e:', ['init=','user=','repo=','event='])
+    options, args = getopt.getopt(sys.argv[1:], 'i:u:r:e:', ['init=', 'user=', 'repo=', 'event='])
     if options in ('-i', '--init'):
         readjson(options[0][1])
         print(0)
         exit()
     else:
-        with open("data.json",encoding='utf-8') as f:
+        with open("data.json", encoding='utf-8') as f:
             for line in f:
                 data.append(json.loads((line)))
     for name, value in options:
@@ -48,7 +49,7 @@ def main():
             reponame = value
         if name in ('-e'):
             eventname = value
-    print(calculate_result(data,username,reponame,eventname))
+    print(calculate_result(data, username, reponame, eventname))
 
 
 if __name__ == "__main__":
